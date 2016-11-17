eb539f3a5cb7a55af19dc97f9cdab9c889ff90a6
Ben Ratliff <bratliff@amplify.com>
2016-11-17 13:20:51 -0500

    stopped wings and added wink

--- js/animation-events.js
+++ js/animation-events.js
@@ -4,20 +4,26 @@ var owlAnim = {
     _this : this,
     owly : $('.owly'),
     bgarray : ['owly','assessment-window','clouds','mountains','trees','tree','grass1','grass2'],
-    owlyFly : function(el, owlWidget) {
-        console.log(owlWidget);
+    owlyFly : function(el, owlWidget, array) {
+        owlWidget.state.setAnimation(0, "flapwings", true);
+        $.each(array, function(i, val){
+            $('.' + val).addClass('transition');
+        });
         {
             var tl = new TimelineLite();
-            tl.to(el, 0.2, {rotation:50, x:5, y:-5});
-            tl.to(el, 0.6, {x:1300, y:-250});
+            tl.to(el, 0.2, {rotation:50, x:5, y:-5})
+            tl.to(el, 0.6, {x:1300, y:-250})
             tl.to(el, 0.2, {rotation:20});
-            tl.to(el, 0.6, {x:1700, y:-400});
-            tl.to(el, 0.2, {rotation:90});
-            tl.to(el, 0.3, {x:2000, y:-300});
-            tl.to(el, 0.3, {rotation:0, x:2100, y:-400});
-            tl.to(el, 0.8, {x:2100, y:0});
+            tl.to(el, 0.6, {x:1700, y:-400})
+            tl.to(el, 0.2, {rotation:90})
+            tl.to(el, 0.3, {x:2000, y:-300})
+            tl.to(el, 0.3, {rotation:0, x:2100, y:-400})
+            tl.to(el, 0.8, {x:2100, y:0})
+            .add(function() {
+                owlWidget.state.setAnimation(0, "wink", false);
+            });
+
         };
-        owlWidget.state.setAnimation(0, "wink", false);
     },
     transition : function(array, owlWidget){
         this.owlyFly(this.owly, owlWidget);
@@ -29,7 +35,7 @@ var owlAnim = {
     init : function(owlWidget) {
         var that = this;
         $('.ok-button').click(function(){
-            that.transition(that.bgarray, owlWidget);
+            that.owlyFly(that.owly, owlWidget, that.bgarray);
 
         });
         $('.sound-button-select').click(function(){
